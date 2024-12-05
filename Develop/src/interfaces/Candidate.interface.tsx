0b03c4bbf2bdf5b6b62a0bb9fae427d426b215ad
melody605 <melodyvalentine9@gmail.com>
2024-12-04 22:13:00 -0500

    searchupdate

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,12 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
-      readonly name: string | null;
-      readonly username: string;
-      readonly location: string | null;
-      readonly bio: string | null;
-      readonly avatar_url: string | null;
-      readonly email: string | null;
-      readonly html_url: string | null;
-      readonly company: string | null;
-    }
+        name: string;
+        username: string;
+        location: string;
+        bio: string;
+        avatar_url: string;
+        email: string;
+        html_url: string;
+        company: string;
+      }
+      
+    
     
--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,7 +1,39 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+
+  const [setCurrentCandidate, currentCandidate, ] = useState<Candidate>({
+    name: string;
+    username: string;
+    location: string;
+    bio: string;
+    avatar_url: string;
+    email: string;
+    html_url: string;
+    company: string;
+  });
+  
+} catch (err) {
+  console.error('An error occurred while fetching candidate data:', err);
+}
+};
+  useEffect(() => { const fetchCandidates = async () => { 
+    const users = await searchGithub(); if (users.length) { 
+    const user = await searchGithubUser(users[Math.floor(Math.random() * 
+      users.length)].username); 
+      setCurrentCandidate(user); } else { console.warn('No users found'); } };
+
+      fetchCandidates();
+    }, []);
+
+      useEffect(() => { 
+        localStorage.setItem('potentialCandidates', JSON.stringify(potentialCandidates)); }, [potentialCandidates]); 
+        
+        const handleCandidateAction = (save: boolean) => { if (save && currentCandidate) { setPotentialCandidates(prev => [...prev, currentCandidate]); } showNextCandidate(); };
+
+
   return <h1>CandidateSearch</h1>;
 };
 
