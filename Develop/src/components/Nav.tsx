078797f22e406d34d66a62539ad58a0cb8e05475
melody605 <melodyvalentine9@gmail.com>
2024-12-04 21:49:38 -0500

    navinterface

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,5 +1,43 @@
+import { Link, useLocation } from react-router-dom
+
+
 const Nav = () => {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <nav className="nav">
+      <p className="nav-item">
+        <Link
+          to="/"
+          ClassName={currentPage === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </p>
+      <p className="nav-item">
+        <Link
+          to="/SavedCandidates"
+          className={
+            currentPage === "/SavedCandidates" ? "nav-link active" : "nav-link"}>
+            Potential Candidates
+          </Link>
+        </p>
+      </nav>
+    );
+  };
+  
+
+
+
+
   // TODO: Add necessary code to display the navigation bar and link between the pages
+
+
+
+
+
+
+
+  
   return (
     <div>Nav</div>
   )
--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+      readonly name: string | null;
+      readonly username: string;
+      readonly location: string | null;
+      readonly bio: string | null;
+      readonly avatar_url: string | null;
+      readonly email: string | null;
+      readonly html_url: string | null;
+      readonly company: string | null;
+    }
+    
